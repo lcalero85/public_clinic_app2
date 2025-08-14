64fb015ce69473701b3c53e5da2e84a2ea96cbce
lcalero85 <53928171+lcalero85@users.noreply.github.com>
2025-08-14 11:51:25 -0600

    Refactor UsersController for improved readability
    Reformatted code for better readability and consistency, including spacing, indentation, and array formatting. Added 'rol' field to registration logic and ensured patient records are created for new users with 'Patients' role. No functional changes were made to controller logic.

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -1,35 +1,41 @@
-<?php 
+<?php
+
 /**
  * Users Page Controller
  * @category  Controller
  */
-class UsersController extends SecureController{
-	function __construct(){
+class UsersController extends SecureController
+{
+	function __construct()
+	{
 		parent::__construct();
 		$this->tablename = "users";
 	}
 	/**
-     * List page records
-     * @param $fieldname (filter record by a field) 
-     * @param $fieldvalue (filter field value)
-     * @return BaseView
-     */
-	function index($fieldname = null , $fieldvalue = null){
+	 * List page records
+	 * @param $fieldname (filter record by a field) 
+	 * @param $fieldvalue (filter field value)
+	 * @return BaseView
+	 */
+	function index($fieldname = null, $fieldvalue = null)
+	{
 		$request = $this->request;
 		$db = $this->GetModel();
 		$tablename = $this->tablename;
-		$fields = array("id_user", 
-			"full_names", 
-			"rol", 
-			"user_name", 
-			"email", 
-			"photo", 
-			"register_date", 
-			"update_date");
+		$fields = array(
+			"id_user",
+			"full_names",
+			"rol",
+			"user_name",
+			"email",
+			"photo",
+			"register_date",
+			"update_date"
+		);
 		$pagination = $this->get_pagination(MAX_RECORD_COUNT); // get current pagination e.g array(page_number, page_limit)
 		//search table record
-		if(!empty($request->search)){
-			$text = trim($request->search); 
+		if (!empty($request->search)) {
+			$text = trim($request->search);
 			$search_condition = "(
 				users.id_user LIKE ? OR 
 				users.full_names LIKE ? OR 
@@ -43,23 +49,31 @@ class UsersController extends SecureController{
 				users.cel LIKE ?
 			)";
 			$search_params = array(
-				"%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%","%$text%"
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%",
+				"%$text%"
 			);
 			//setting search conditions
 			$db->where($search_condition, $search_params);
-			 //template to use when ajax search
+			//template to use when ajax search
 			$this->view->search_template = "users/search.php";
 		}
-		if(!empty($request->orderby)){
+		if (!empty($request->orderby)) {
 			$orderby = $request->orderby;
 			$ordertype = (!empty($request->ordertype) ? $request->ordertype : ORDER_TYPE);
 			$db->orderBy($orderby, $ordertype);
-		}
-		else{
+		} else {
 			$db->orderBy("users.id_user", ORDER_TYPE);
 		}
-		if($fieldname){
-			$db->where($fieldname , $fieldvalue); //filter by a single field name
+		if ($fieldname) {
+			$db->where($fieldname, $fieldvalue); //filter by a single field name
 		}
 		$tc = $db->withTotalCount();
 		$records = $db->get($tablename, $pagination, $fields);
@@ -75,7 +89,7 @@ class UsersController extends SecureController{
 		//list of record id for audit trail
 		$this->rec_id = array_column($records, "id_user");
 		$this->write_to_log("list", "true");
-		if($db->getLastError()){
+		if ($db->getLastError()) {
 			$this->set_page_error();
 			$this->write_to_log("list", "false");
 		}
@@ -88,44 +102,44 @@ class UsersController extends SecureController{
 		$this->render_view("users/list.php", $data); //render the full page
 	}
 	/**
-     * View record detail 
+	 * View record detail 
 	 * @param $rec_id (select record by table primary key) 
-     * @param $value value (select record by value of field name(rec_id))
-     * @return BaseView
-     */
-	function view($rec_id = null, $value = null){
+	 * @param $value value (select record by value of field name(rec_id))
+	 * @return BaseView
+	 */
+	function view($rec_id = null, $value = null)
+	{
 		$request = $this->request;
 		$db = $this->GetModel();
 		$rec_id = $this->rec_id = urldecode($rec_id);
 		$tablename = $this->tablename;
-		$fields = array("id_user", 
-			"full_names", 
-			"rol", 
-			"user_name", 
-			"email", 
-			"register_date", 
-			"update_date");
-		if($value){
+		$fields = array(
+			"id_user",
+			"full_names",
+			"rol",
+			"user_name",
+			"email",
+			"register_date",
+			"update_date"
+		);
+		if ($value) {
 			$db->where($rec_id, urldecode($value)); //select record based on field name
-		}
-		else{
+		} else {
 			$db->where("users.id_user", $rec_id);; //select record based on primary key
 		}
-		$record = $db->getOne($tablename, $fields );
-		if($record){
+		$record = $db->getOne($tablename, $fields);
+		if ($record) {
 			$this->write_to_log("view", "true");
 			$page_title = $this->view->page_title = "View  Users";
-		$this->view->report_filename = date('Y-m-d') . '-' . $page_title;
-		$this->view->report_title = $page_title;
-		$this->view->report_layout = "report_layout.php";
-		$this->view->report_paper_size = "A4";
-		$this->view->report_orientation = "portrait";
-		}
-		else{
-			if($db->getLastError()){
+			$this->view->report_filename = date('Y-m-d') . '-' . $page_title;
+			$this->view->report_title = $page_title;
+			$this->view->report_layout = "report_layout.php";
+			$this->view->report_paper_size = "A4";
+			$this->view->report_orientation = "portrait";
+		} else {
+			if ($db->getLastError()) {
 				$this->set_page_error();
-			}
-			else{
+			} else {
 				$this->set_page_error("No record found");
 			}
 			$this->write_to_log("view", "false");
@@ -133,21 +147,22 @@ class UsersController extends SecureController{
 		return $this->render_view("users/view.php", $record);
 	}
 	/**
-     * Insert new record to the database table
+	 * Insert new record to the database table
 	 * @param $formdata array() from $_POST
-     * @return BaseView
-     */
-	function add($formdata = null){
-		if($formdata){
+	 * @return BaseView
+	 */
+	function add($formdata = null)
+	{
+		if ($formdata) {
 			$db = $this->GetModel();
 			$tablename = $this->tablename;
 			$request = $this->request;
 			//fillable fields
-			$fields = $this->fields = array("full_names","rol","user_name","password","email","photo","register_date","update_date");
+			$fields = $this->fields = array("full_names", "rol", "user_name", "password", "email", "photo", "register_date", "update_date");
 			$postdata = $this->format_request_data($formdata);
 			$cpassword = $postdata['confirm_password'];
 			$password = $postdata['password'];
-			if($cpassword != $password){
+			if ($cpassword != $password) {
 				$this->view->page_error[] = "Your password confirmation is not consistent";
 			}
 			$this->rules_array = array(
@@ -169,27 +184,26 @@ class UsersController extends SecureController{
 			$modeldata = $this->modeldata = $this->validate_form($postdata);
 			$password_text = $modeldata['password'];
 			//update modeldata with the password hash
-			$modeldata['password'] = $this->modeldata['password'] = password_hash($password_text , PASSWORD_DEFAULT);
+			$modeldata['password'] = $this->modeldata['password'] = password_hash($password_text, PASSWORD_DEFAULT);
 			$modeldata['register_date'] = datetime_now();
-$modeldata['update_date'] = datetime_now();
+			$modeldata['update_date'] = datetime_now();
 			//Check if Duplicate Record Already Exit In The Database
 			$db->where("user_name", $modeldata['user_name']);
-			if($db->has($tablename)){
-				$this->view->page_error[] = $modeldata['user_name']." Already exist!";
+			if ($db->has($tablename)) {
+				$this->view->page_error[] = $modeldata['user_name'] . " Already exist!";
 			}
 			//Check if Duplicate Record Already Exit In The Database
 			$db->where("email", $modeldata['email']);
-			if($db->has($tablename)){
-				$this->view->page_error[] = $modeldata['email']." Already exist!";
-			} 
-			if($this->validated()){
+			if ($db->has($tablename)) {
+				$this->view->page_error[] = $modeldata['email'] . " Already exist!";
+			}
+			if ($this->validated()) {
 				$rec_id = $this->rec_id = $db->insert($tablename, $modeldata);
-				if($rec_id){
+				if ($rec_id) {
 					$this->write_to_log("add", "true");
 					$this->set_flash_msg("Record added successfully", "success");
 					return	$this->redirect("users");
-				}
-				else{
+				} else {
 					$this->set_page_error();
 					$this->write_to_log("add", "false");
 				}
@@ -199,19 +213,20 @@ $modeldata['update_date'] = datetime_now();
 		$this->render_view("users/add.php");
 	}
 	/**
-     * Update table record with formdata
+	 * Update table record with formdata
 	 * @param $rec_id (select record by table primary key)
 	 * @param $formdata array() from $_POST
-     * @return array
-     */
-	function edit($rec_id = null, $formdata = null){
+	 * @return array
+	 */
+	function edit($rec_id = null, $formdata = null)
+	{
 		$request = $this->request;
 		$db = $this->GetModel();
 		$this->rec_id = $rec_id;
 		$tablename = $this->tablename;
-		 //editable fields
-		$fields = $this->fields = array("id_user","full_names","rol","user_name","photo","register_date","update_date");
-		if($formdata){
+		//editable fields
+		$fields = $this->fields = array("id_user", "full_names", "rol", "user_name", "photo", "register_date", "update_date");
+		if ($formdata) {
 			$postdata = $this->format_request_data($formdata);
 			$this->rules_array = array(
 				'full_names' => 'required',
@@ -227,29 +242,27 @@ $modeldata['update_date'] = datetime_now();
 			);
 			$modeldata = $this->modeldata = $this->validate_form($postdata);
 			$modeldata['register_date'] = datetime_now();
-$modeldata['update_date'] = datetime_now();
+			$modeldata['update_date'] = datetime_now();
 			//Check if Duplicate Record Already Exit In The Database
-			if(isset($modeldata['user_name'])){
+			if (isset($modeldata['user_name'])) {
 				$db->where("user_name", $modeldata['user_name'])->where("id_user", $rec_id, "!=");
-				if($db->has($tablename)){
-					$this->view->page_error[] = $modeldata['user_name']." Already exist!";
+				if ($db->has($tablename)) {
+					$this->view->page_error[] = $modeldata['user_name'] . " Already exist!";
 				}
-			} 
-			if($this->validated()){
+			}
+			if ($this->validated()) {
 				$db->where("users.id_user", $rec_id);;
 				$bool = $db->update($tablename, $modeldata);
 				$numRows = $db->getRowCount(); //number of affected rows. 0 = no record field updated
-				if($bool && $numRows){
+				if ($bool && $numRows) {
 					$this->write_to_log("edit", "true");
 					$this->set_flash_msg("Record updated successfully", "success");
 					return $this->redirect("users");
-				}
-				else{
-					if($db->getLastError()){
+				} else {
+					if ($db->getLastError()) {
 						$this->set_page_error();
 						$this->write_to_log("edit", "false");
-					}
-					elseif(!$numRows){
+					} elseif (!$numRows) {
 						//not an error, but no record was updated
 						$page_error = "No record updated";
 						$this->set_page_error($page_error);
@@ -263,17 +276,18 @@ $modeldata['update_date'] = datetime_now();
 		$db->where("users.id_user", $rec_id);;
 		$data = $db->getOne($tablename, $fields);
 		$page_title = $this->view->page_title = "Edit  Users";
-		if(!$data){
+		if (!$data) {
 			$this->set_page_error();
 		}
 		return $this->render_view("users/edit.php", $data);
 	}
 	/**
-     * Delete record from the database
+	 * Delete record from the database
 	 * Support multi delete by separating record id by comma.
-     * @return BaseView
-     */
-	function delete($rec_id = null){
+	 * @return BaseView
+	 */
+	function delete($rec_id = null)
+	{
 		Csrf::cross_check();
 		$request = $this->request;
 		$db = $this->GetModel();
@@ -283,11 +297,10 @@ $modeldata['update_date'] = datetime_now();
 		$arr_rec_id = array_map('trim', explode(",", $rec_id));
 		$db->where("users.id_user", $arr_rec_id, "in");
 		$bool = $db->delete($tablename);
-		if($bool){
+		if ($bool) {
 			$this->write_to_log("delete", "true");
 			$this->set_flash_msg("Record deleted successfully", "success");
-		}
-		elseif($db->getLastError()){
+		} elseif ($db->getLastError()) {
 			$page_error = $db->getLastError();
 			$this->set_flash_msg($page_error, "danger");
 			$this->write_to_log("delete", "false");
@@ -295,21 +308,31 @@ $modeldata['update_date'] = datetime_now();
 		return	$this->redirect("users");
 	}
 	/**
-     * Insert new record to the database table
+	 * Insert new record to the database table
 	 * @param $formdata array() from $_POST
-     * @return BaseView
-     */
-	function register($formdata = null){
-		if($formdata){
+	 * @return BaseView
+	 */
+	function register($formdata = null)
+	{
+		if ($formdata) {
 			$db = $this->GetModel();
 			$tablename = $this->tablename;
 			$request = $this->request;
 			//fillable fields
-			$fields = $this->fields = array("full_names","user_name","password","email","register_date","update_date","cel");
+			$fields = $this->fields = array(
+				"full_names",
+				"user_name",
+				"password",
+				"email",
+				"register_date",
+				"update_date",
+				"cel",
+				"rol",
+			);
 			$postdata = $this->format_request_data($formdata);
 			$cpassword = $postdata['confirm_password'];
 			$password = $postdata['password'];
-			if($cpassword != $password){
+			if ($cpassword != $password) {
 				$this->view->page_error[] = "Your password confirmation is not consistent";
 			}
 			$this->rules_array = array(
@@ -320,6 +343,8 @@ $modeldata['update_date'] = datetime_now();
 				'register_date' => 'required',
 				'update_date' => 'required',
 				'cel' => 'required',
+				'rol' => '',
+
 			);
 			$this->sanitize_array = array(
 				'full_names' => 'sanitize_string',
@@ -328,41 +353,43 @@ $modeldata['update_date'] = datetime_now();
 				'register_date' => 'sanitize_string',
 				'update_date' => 'sanitize_string',
 				'cel' => 'sanitize_string',
+				'rol'=> '',
 			);
 			$this->filter_vals = true; //set whether to remove empty fields
 			$modeldata = $this->modeldata = $this->validate_form($postdata);
+			$modeldata['rol'] = 'Patients';
 			$password_text = $modeldata['password'];
+
 			//update modeldata with the password hash
-			$modeldata['password'] = $this->modeldata['password'] = password_hash($password_text , PASSWORD_DEFAULT);
+			$modeldata['password'] = $this->modeldata['password'] = password_hash($password_text, PASSWORD_DEFAULT);
 			//Check if Duplicate Record Already Exit In The Database
 			$db->where("user_name", $modeldata['user_name']);
-			if($db->has($tablename)){
-				$this->view->page_error[] = $modeldata['user_name']." Already exist!";
+			if ($db->has($tablename)) {
+				$this->view->page_error[] = $modeldata['user_name'] . " Already exist!";
 			}
 			//Check if Duplicate Record Already Exit In The Database
 			$db->where("email", $modeldata['email']);
-			if($db->has($tablename)){
-				$this->view->page_error[] = $modeldata['email']." Already exist!";
-			} 
-			if($this->validated()){
+			if ($db->has($tablename)) {
+				$this->view->page_error[] = $modeldata['email'] . " Already exist!";
+			}
+			if ($this->validated()) {
 				$rec_id = $this->rec_id = $db->insert($tablename, $modeldata);
-				if($rec_id){
+				if ($rec_id) {
 					$this->write_to_log("add", "true");
-		# Statement to execute after adding record
-// For each register of patients rol 
-$table_data = array(
-    "id_user" => $rec_id,
-    "full_names" =>$modeldata['full_names'],
-    "email"=>$modeldata['email'],
-    "phone_patient"=>$modeldata['cel'],
-    "id_status"=>1
-);
-$db->insert("clinic_patients", $table_data);
-		# End of after add statement
+					# Statement to execute after adding record
+					// For each register of patients rol 
+					$table_data = array(
+						"id_user" => $rec_id,
+						"full_names" => $modeldata['full_names'],
+						"email" => $modeldata['email'],
+						"phone_patient" => $modeldata['cel'],
+						"id_status" => 1
+					);
+					$db->insert("clinic_patients", $table_data);
+					# End of after add statement
 					$this->set_flash_msg("Grabar agregado exitosamente", "success");
 					return	$this->redirect("users");
-				}
-				else{
+				} else {
 					$this->set_page_error();
 					$this->write_to_log("add", "false");
 				}
